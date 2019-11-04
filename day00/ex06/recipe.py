6566be31c13c01508627f8e34a0829f0760fa2e1
Ant0wan <abarthel@student.42.fr>
2019-11-04 19:52:10 +0100

    fix(init ex06): dict added

--- day00/ex06/recipe.py
+++ day00/ex06/recipe.py
@@ -1,40 +1,33 @@
 #!/usr/local/bin/python3.7
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    recipe.py                                          :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2019/11/04 18:36:44 by abarthel          #+#    #+#              #
-#    Updated: 2019/11/04 18:36:45 by abarthel         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
+# *************************************************************************** #
+#                                                                             #
+#                                                        :::      ::::::::    #
+#   recipe.py                                          :+:      :+:    :+:    #
+#                                                    +:+ +:+         +:+      #
+#   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
+#                                                +#+#+#+#+#+   +#+            #
+#   Created: 2019/11/04 18:36:44 by abarthel          #+#    #+#              #
+#   Updated: 2019/11/04 18:36:45 by abarthel         ###   ########.fr        #
+#                                                                             #
+# *************************************************************************** #
 
-ingredients = {
-        'sandwich':['ham', 'bread', 'cheese', 'tomatoes'],
-        'cake':['flour', 'sugar', 'eggs'],
-        'salad':['avocado', 'arugula', 'tomatoes', 'spinach']
+cookbook = {
+        'sandwich': {'ingredients': ['ham', 'bread', 'cheese', 'tomatoes'], 'meal': 'lunch', 'prep_time': '10'},
+        'cake': {'ingredients': ['flour', 'sugar', 'eggs'], 'meal': 'dessert', 'prep_time': '60'},
+        'salad': {'ingredients': ['avocado', 'arugula', 'tomatoes', 'spinach'], 'meal': 'lunch', 'prep_time': '15'}
     }
 
-dish = {
-        'sandwich':'lunch',
-        'cake':'dessert',
-        'salad':'lunch'
-    }
-
-time = {
-        'sandwich':10,
-        'cake':60,
-        'salad':15
-    }
 
 def recipe(name):
-    print('Recipe for {0}:\nIngredients list: {1}\nTo be eaten for {2}.\nTakes {3} minutes of cooking.'.format(name, ingredients[name], dish[name], time[name]))
+    print('Recipe for {0}:\nIngredients list:\
+ {1}\nTo be eaten for {2}.\nTakes {3} minutes\
+ of cooking.'.format(name, cookbook[name]['ingredients'], cookbook[name]['meal'], cookbook[name]['prep_time']), end='\n')
+
 
 if __name__ == '__main__':
-    name = input('Please enter the recipe\'s name to get its details:\n>> ')    
+    name = input('Please enter the recipe\'s name to get its details:\n>> ')
     try:
         recipe(name)
-    except:
-        exit()
+    except KeyError:
+        print('This option does not exist, please type the\
+ corresponding number.\nTo exit, enter 5.', end='\n')
