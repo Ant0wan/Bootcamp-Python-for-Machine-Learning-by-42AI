e589a1aa34c4c4efde71eb1fcedaf653f2867489
Ant0wan <abarthel@student.42.fr>
2019-11-04 13:14:21 +0100

    fet(count): gp

---
+++ day00/ex03/count.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/python3.7
+# *************************************************************************** #
+#                                                                             #
+#                                                        :::      ::::::::    #
+#   count.py                                           :+:      :+:    :+:    #
+#                                                    +:+ +:+         +:+      #
+#   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
+#                                                +#+#+#+#+#+   +#+            #
+#   Created: 2019/11/04 12:35:58 by abarthel          #+#    #+#              #
+#   Updated: 2019/11/04 12:35:58 by abarthel         ###   ########.fr        #
+#                                                                             #
+# *************************************************************************** #
+
+def output_analysis(c, ul, ll, pm, sp):
+	print('The text contains {0} characters:\n- {1} upper letters\n- {2} lower letters\n- {3} punctuation marks\n- {4} spaces\n'.format(c, ul, ll, pm, sp))
+
+def text_analyzer(text):
+    for i in text:
+       f 
+
+
