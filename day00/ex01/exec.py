78d0af4630b94ed3c9f128a446a86eac5c35e4a0
Ant0wan <abarthel@student.42.fr>
2019-11-04 11:25:34 +0100

    norm on ex01

--- day00/ex01/exec.py
+++ day00/ex01/exec.py
@@ -1,24 +1,26 @@
 #!/usr/local/bin/python3.7
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    exec.py                                            :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2019/11/04 09:36:12 by abarthel          #+#    #+#              #
-#    Updated: 2019/11/04 09:36:14 by abarthel         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
+# *************************************************************************** #
+#                                                                             #
+#                                                        :::      ::::::::    #
+#   exec.py                                            :+:      :+:    :+:    #
+#                                                    +:+ +:+         +:+      #
+#   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
+#                                                +#+#+#+#+#+   +#+            #
+#   Created: 2019/11/04 09:36:12 by abarthel          #+#    #+#              #
+#   Updated: 2019/11/04 09:36:14 by abarthel         ###   ########.fr        #
+#                                                                             #
+# *************************************************************************** #
 
 import sys
 
+
 def rev_alpha():
-	if len(sys.argv) > 1:
-		list = []
-		for i in sys.argv[:0:-1]:
-			list.append((i[::-1].swapcase()))
-		print(' '.join(list), end='\n')
+    if len(sys.argv) > 1:
+        list = []
+        for i in sys.argv[:0:-1]:
+            list.append((i[::-1].swapcase()))
+            print(' '.join(list), end='\n')
+
 
 if __name__ == '__main__':
-	rev_alpha()
+    rev_alpha()
