a2a779307ce783575b06a1c5d8e6fdc065561d18
Ant0wan <abarthel@student.42.fr>
2019-11-04 18:54:12 +0100

    feat(init ex06): dict added

---
+++ day00/ex06/recipe.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python3.7
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    recipe.py                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2019/11/04 18:36:44 by abarthel          #+#    #+#              #
+#    Updated: 2019/11/04 18:36:45 by abarthel         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+ingredients = {
+        'sandwich':[ 'ham', 'bread', 'cheese', 'tomatoes'],
+        'cake':['flour', 'sugar', 'eggs'],
+        'salad':['avocado', 'arugula', 'tomatoes', 'spinach']
+    }
+
+meal = {
+        'sandwich':'lunch',
+        'cake':'dessert',
+        'salad':'lunch'
+    }
+
+time = {
+        'sandwich':10,
+        'cake':60,
+        'salad':15
+    }
+
+if __name__ == '__main__':
+    print('ok')
