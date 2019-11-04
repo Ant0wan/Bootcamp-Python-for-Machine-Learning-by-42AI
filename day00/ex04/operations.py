a18076ca971709d965d75c0d53f977817c7e2d8f
Ant0wan <abarthel@student.42.fr>
2019-11-04 16:53:35 +0100

    feat(ex04): operation with float

--- day00/ex04/operations.py
+++ day00/ex04/operations.py
@@ -11,13 +11,29 @@
 #                                                                             #
 # *************************************************************************** #
 
-import sys, math
+import sys
+import math
 
+def usage():
+    print('Usage: python operations.py\nExample:\n\tpython3.7 operations.py 10 3', end='\n')
+    
 
 if __name__ == '__main__':
     argc = len(sys.argv)
-    if argc is 3 and sys.argv[1].isdigit and sys.argv[2].isdigit:
-        a = int(sys.argv[1])
-        b = int(sys.argv[2])
-        print('Sum: {:>20}'.format(a + b))
-        print('Difference: {:>13}'.format(a - b))
+    if argc > 3:
+        print('InputError: too many arguments', end='\n')
+        usage()
+    elif argc is 3:
+        try:
+            a = float(sys.argv[1])
+            b = float(sys.argv[2])
+            print('Sum: {:>20}'.format(math.fsum([a, b])), end='\n')
+            print('Difference: {:>13}'.format(a - b), end='\n')
+            print('Product: {:>16}'.format(a * b), end='\n')
+            print('Quotient: {:>15}'.format(a / b), end='\n')
+            print('Remainder: {:>14}'.format(a % b), end='\n')
+        except:
+            print('InputError: only numbers', end='\n')
+            usage()
+    else:
+        usage()
