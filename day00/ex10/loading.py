9c38fc0ab703a92da966e73611765ded7698680d
Ant0wan <abarthel@student.42.fr>
2019-11-05 14:54:24 +0100

    fix(loading.py): on the go ex10

--- day00/ex10/loading.py
+++ day00/ex10/loading.py
@@ -11,4 +11,22 @@
 #                                                                             #
 # *************************************************************************** #
 
-if __name__ == '__main__':
+from time import sleep
+import sys
+'''
+ETA: 8.67s [ 23%][=====>                 ] 233/1000 | elapsed time 2.33s
+'''
+
+def ft_progress(listy):
+    for i in listy:
+        print("{:4.0%}".format((i + 1)/len(listy)), end='\r')
+        yield i
+
+
+listy = range(100)
+ret = 0
+for elem in ft_progress(listy):
+    ret += (elem + 3) % 5
+    sleep(0.01)
+print()
+print(ret)
