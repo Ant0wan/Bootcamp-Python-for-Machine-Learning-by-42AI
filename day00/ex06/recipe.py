cf2353c3b5bde95ebf1b0680745ca0b5ee724bc3
Antoine Barthelemy <antoine@linux.com>
2022-01-13 17:30:07 +0100

    day00 ex06

--- day00/ex06/recipe.py
+++ day00/ex06/recipe.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3
-# *************************************************************************** #
-#                                                                             #
-#                                                        :::      ::::::::    #
-#   recipe.py                                          :+:      :+:    :+:    #
-#                                                    +:+ +:+         +:+      #
-#   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
-#                                                +#+#+#+#+#+   +#+            #
-#   Created: 2019/11/04 18:36:44 by abarthel          #+#    #+#              #
-#   Updated: 2019/11/04 18:36:45 by abarthel         ###   ########.fr        #
-#                                                                             #
-# *************************************************************************** #
+"""
+recipe
+"""
 
-cookbook = {
+COOKBOOK = {
         'sandwich': {
             'ingredients': [
                 'ham',
@@ -45,50 +37,55 @@ cookbook = {
 
 
 def quit_fn():
+    """quit"""
     raise SystemExit
 
 
 def print_a_recipe():
+    """Print recipe"""
     try:
         name = input('Please enter the recipe\'s \
                 name to get its details:\n>> ')
         print('Recipe for {0}:\nIngredients list:\
  {1}\nTo be eaten for {2}.\nTakes {3} minutes\
  of cooking.'.format(
-                        name,
-                        cookbook[name]['ingredients'],
-                        cookbook[name]['meal'],
-                        cookbook[name]['prep_time']), end='\n')
+     name,
+     COOKBOOK[name]['ingredients'],
+     COOKBOOK[name]['meal'],
+     COOKBOOK[name]['prep_time']), end='\n')
     except KeyError:
         print(name + ' is not in the cookbook.', end='\n')
 
 
 def delete_a_recipe():
+    """Delete recipe"""
     name = input('What recipe do you want to delete ?\n>> ')
     try:
-        del cookbook[name]
+        del COOKBOOK[name]
     except KeyError:
         print(name + ' is not in the cookbook.', end='\n')
 
 
 def add_a_new_recipe():
+    """Add a recipe"""
     name = input('Recipe name\n>> ')
     ingredients = input('List ingredients\n>> ')
     meal = input('What dish or meal time is it for ?\n>> ')
     prep_time = input('How long does it take to cook it ?\n>> ')
-    cookbook[name] = {
-            'ingredients': ingredients,
-            'meal': meal,
-            'prep_time': prep_time
-            }
+    COOKBOOK[name] = {
+        'ingredients': ingredients,
+        'meal': meal,
+        'prep_time': prep_time
+    }
 
 
 def print_all_recipe_name():
-    for i in cookbook.keys():
-        print(i)
+    """Display all recipe"""
+    for recipe in COOKBOOK:
+        print(recipe)
 
 
-menu = {
+MENU = {
         '1': ('Add a recipe', add_a_new_recipe),
         '2': ('Delete a recipe', delete_a_recipe),
         '3': ('Print a recipe', print_a_recipe),
@@ -100,11 +97,11 @@ menu = {
 if __name__ == '__main__':
     while True:
         print('Please select an option by typing the corresponding number:')
-        for i in menu:
-            print(i, menu[i][0], sep=': ', end='\n')
-        choice = input('>> ')
+        for i in MENU:
+            print(i, MENU[i][0], sep=': ', end='\n')
+        CHOICE = input('>> ')
         try:
-            menu[choice][1]()
+            MENU[CHOICE][1]()
         except KeyError:
             print('This option does not exist, please type the\
  corresponding number.\nTo exit, enter 5.', end='\n')
