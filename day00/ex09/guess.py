a452ddf454adba47c6aa74ee26f8f9e24fa531b5
Antoine Barthelemy <antoine@linux.com>
2022-01-13 19:25:15 +0100

    day00 ex09

--- day00/ex09/guess.py
+++ day00/ex09/guess.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
-# *************************************************************************** #
-#                                                                             #
-#                                                        :::      ::::::::    #
-#   guess.py                                           :+:      :+:    :+:    #
-#                                                    +:+ +:+         +:+      #
-#   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
-#                                                +#+#+#+#+#+   +#+            #
-#   Created: 2019/11/05 12:21:58 by abarthel          #+#    #+#              #
-#   Updated: 2019/11/05 12:21:58 by abarthel         ###   ########.fr        #
-#                                                                             #
-# *************************************************************************** #
+"""
+guess
+"""
 
 from random import randint
+import sys
 
 
 def intro():
+    """Introduction"""
     print('This is an interactive guessing game!',
           'You have to enter a number between 1 and 99\
  to find out the secret number.',
@@ -23,20 +17,20 @@ def intro():
 
 if __name__ == '__main__':
     intro()
-    number, user_input, attempt = randint(1, 99), 0, 0
-    while int(user_input) is not int(number):
+    number, USER_INPUT, attempt = randint(1, 99), 0, 0
+    while int(USER_INPUT) is not int(number):
         try:
-            user_input = input('What\'s your guess between 1 and 99?\n>> ')
-            if user_input == 'exit':
+            USER_INPUT = input('What\'s your guess between 1 and 99?\n>> ')
+            if USER_INPUT == 'exit':
                 print('Goodbye!', end='\n')
-                exit()
-            elif int(user_input) > number:
+                sys.exit()
+            elif int(USER_INPUT) > number:
                 print('Too high!', end='\n')
-            elif int(user_input) < number:
+            elif int(USER_INPUT) < number:
                 print('Too low!', end='\n')
         except ValueError:
             print('That\'s not a number.', end='\n')
-            user_input = 0
+            USER_INPUT = 0
         attempt += 1
     print('Congratulations, you\'ve got it!',
-          'You won in {0} attempts!'.format(attempt), sep='\n', end='\n')
+          f'You won in {attempt} attempts!', sep='\n', end='\n')
