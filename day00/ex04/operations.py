f97c2469e1d4c99536950af0ce67089d43289636
Antoine Barthelemy <antoine@linux.com>
2022-01-13 16:41:17 +0100

    lint(day00): ex04

--- day00/ex04/operations.py
+++ day00/ex04/operations.py
@@ -1,55 +1,47 @@
 #!/usr/bin/env python3
-# *************************************************************************** #
-#                                                                             #
-#                                                        :::      ::::::::    #
-#   operations.py                                      :+:      :+:    :+:    #
-#                                                    +:+ +:+         +:+      #
-#   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
-#                                                +#+#+#+#+#+   +#+            #
-#   Created: 2019/11/04 15:03:42 by abarthel          #+#    #+#              #
-#   Updated: 2019/11/04 15:03:42 by abarthel         ###   ########.fr        #
-#                                                                             #
-# *************************************************************************** #
+"""
+ex04
+"""
 
 import sys
 import math
 
 
 def usage():
+    """Display usage"""
     print('Usage: python operations.py', end='\n')
     print('Example:\n\tpython3.7 operations.py 10 3', end='\n')
 
 
-def div(a, b):
-    if not b:
-        return ('ERROR (div by zero)')
-    else:
-        return (a / b)
+def div(dividend, divisor):
+    """Divide dividend by divisor"""
+    if not divisor:
+        return 'ERROR (div by zero)'
+    return dividend / divisor
 
 
-def mod(a, b):
-    if not b:
-        return ('ERROR (modulo by zero)')
-    else:
-        return (a % b)
+def mod(dividend, divisor):
+    """Divide dividend modulo divisor"""
+    if not divisor:
+        return 'ERROR (modulo by zero)'
+    return dividend % divisor
 
 
 if __name__ == '__main__':
-    argc = len(sys.argv)
-    if argc > 3:
+    ARGC = len(sys.argv)
+    if ARGC > 3:
         print('InputError: too many arguments', end='\n')
         usage()
-    elif argc == 3:
+    elif ARGC == 3:
         try:
-            a = float(sys.argv[1])
-            b = float(sys.argv[2])
-            print('Sum: {:>20}'.format(math.fsum([a, b])), end='\n')
-            print('Difference: {:>13}'.format(a - b), end='\n')
-            print('Product: {:>16}'.format(a * b), end='\n')
-            print('Quotient: {:>15}'.format(div(a, b)), end='\n')
-            print('Remainder: {:>14}'.format(mod(a, b)), end='\n')
+            ARG_ONE = float(sys.argv[1])
+            ARG_TWO = float(sys.argv[2])
+            print('Sum: {:>20}'.format(math.fsum([ARG_ONE, ARG_TWO])), end='\n')
+            print('Difference: {:>13}'.format(ARG_ONE - ARG_TWO), end='\n')
+            print('Product: {:>16}'.format(ARG_ONE * ARG_TWO), end='\n')
+            print('Quotient: {:>15}'.format(div(ARG_ONE, ARG_TWO)), end='\n')
+            print('Remainder: {:>14}'.format(mod(ARG_ONE, ARG_TWO)), end='\n')
         except ValueError:
             print('InputError: only numbers', end='\n')
             usage()
-    else:
-        usage()
+    usage()
