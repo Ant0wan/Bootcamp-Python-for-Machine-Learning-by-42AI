5c728a95c00a12010ad22f3aa2f79949c06f2cbb
Antoine Barthelemy <antoine@linux.com>
2022-01-13 17:37:02 +0100

    day00 ex07

--- day00/ex07/filterwords.py
+++ day00/ex07/filterwords.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python3
-# *************************************************************************** #
-#                                                                             #
-#                                                        :::      ::::::::    #
-#   filterwords.py                                     :+:      :+:    :+:    #
-#                                                    +:+ +:+         +:+      #
-#   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
-#                                                +#+#+#+#+#+   +#+            #
-#   Created: 2019/11/05 09:57:15 by abarthel          #+#    #+#              #
-#   Updated: 2019/11/05 09:57:15 by abarthel         ###   ########.fr        #
-#                                                                             #
-# *************************************************************************** #
+"""
+filterwords
+"""
 
 import re
 import sys
@@ -17,8 +9,8 @@ import sys
 
 if __name__ == '__main__':
     try:
-        s = re.sub(r'[^\w\s]', '', sys.argv[1])
-        s = re.split(" +", s)
-        print([word for word in s if len(word) > int(sys.argv[2])])
+        STRING = re.sub(r'[^\w\s]', '', sys.argv[1])
+        STRING = re.split(" +", STRING)
+        print([word for word in STRING if len(word) > int(sys.argv[2])])
     except ValueError:
         print('ERROR', end='\n')
