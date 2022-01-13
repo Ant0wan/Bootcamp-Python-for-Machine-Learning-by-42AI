c3baa452f9bf42418e01c5f6d0a59905fbfcee18
Antoine Barthelemy <antoine@linux.com>
2022-01-13 17:18:39 +0100

    lint(day00): ex05 kata02 03 04

--- day00/ex05/kata03.py
+++ day00/ex05/kata03.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3
-# *************************************************************************** #
-#                                                                             #
-#                                                        :::      ::::::::    #
-#   kata03.py                                          :+:      :+:    :+:    #
-#                                                    +:+ +:+         +:+      #
-#   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
-#                                                +#+#+#+#+#+   +#+            #
-#   Created: 2019/11/04 18:22:53 by abarthel          #+#    #+#              #
-#   Updated: 2019/11/04 18:22:53 by abarthel         ###   ########.fr        #
-#                                                                             #
-# *************************************************************************** #
+"""
+kata03
+"""
 
-phrase = "The right format"
+PHRASE = "The right format"
 
 if __name__ == '__main__':
-    print('{0:->42}'.format(phrase), end='')
+    print('{0:->42}'.format(PHRASE), end='')
