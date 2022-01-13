79e2cba941f1e273c13e414b14578e77958cca5d
Antoine Barthelemy <antoine@linux.com>
2022-01-13 16:44:59 +0100

    lint(day00): ex05 kata00 01

--- day00/ex05/kata01.py
+++ day00/ex05/kata01.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python3
-# *************************************************************************** #
-#                                                                             #
-#                                                        :::      ::::::::    #
-#   kata01.py                                          :+:      :+:    :+:    #
-#                                                    +:+ +:+         +:+      #
-#   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
-#                                                +#+#+#+#+#+   +#+            #
-#   Created: 2019/11/04 17:55:28 by abarthel          #+#    #+#              #
-#   Updated: 2019/11/04 17:55:28 by abarthel         ###   ########.fr        #
-#                                                                             #
-# *************************************************************************** #
+"""
+kata01
+"""
 
-languages = {
+LANGUAGES = {
     'Python': 'Guido van Rossum',
     'Ruby': 'Yukihiro Matsumoto',
     'PHP': 'Rasmus Lerdorf',
     }
 
 if __name__ == '__main__':
-    for key, val in languages.items():
+    for key, val in LANGUAGES.items():
         print(key, 'was created by', val, sep=' ', end='\n')
