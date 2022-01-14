367013efbd0f36e809da2c1d38207bbeaefd0f88
Antoine Barthelemy <antoine@linux.com>
2022-01-14 16:03:17 +0100

    pylint module00

--- module_00/ex03/count.py
+++ module_00/ex03/count.py
@@ -6,7 +6,10 @@ ex03
 import string
 
 
-def output_analysis(count, upper_letters, lower_letters, punctuation_marks, spaces):
+def output_analysis(
+        count, upletters, lowletters,
+        punctmarks, spaces
+):
     """Description of output_analysis(...):
 
     This function append all letters in a string
@@ -14,9 +17,9 @@ def output_analysis(count, upper_letters, lower_letters, punctuation_marks, spac
     Returns: no value.
     """
     displayed_list = [f'The text contains {count} characters:']
-    displayed_list.append(f'- {upper_letters} upper letters')
-    displayed_list.append(f'- {lower_letters} lower letters')
-    displayed_list.append(f'- {punctuation_marks} punctuation marks')
+    displayed_list.append(f'- {upletters} upper letters')
+    displayed_list.append(f'- {lowletters} lower letters')
+    displayed_list.append(f'- {punctmarks} punctuation marks')
     displayed_list.append(f'- {spaces} spaces')
     print('\n'.join(displayed_list), end='\n')
 
@@ -33,15 +36,15 @@ def text_analyzer(text=""):
     """
     if not text:
         text = input("What is the text to analyse?\n>> ")
-    count, upper_letters, lower_letters, punctuation_marks, spaces = 0, 0, 0, 0, 0
+    count, upletters, lowletters, punctmarks, spaces = 0, 0, 0, 0, 0
     for i in text:
         count += 1
         if i.isupper():
-            upper_letters += 1
+            upletters += 1
         elif i.islower():
-            lower_letters += 1
+            lowletters += 1
         elif i in string.punctuation:
-            punctuation_marks += 1
+            punctmarks += 1
         elif i == ' ':
             spaces += 1
-    output_analysis(count, upper_letters, lower_letters, punctuation_marks, spaces)
+    output_analysis(count, upletters, lowletters, punctmarks, spaces)
