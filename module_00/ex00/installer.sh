7ab2ffc1fa69b5f98a42202a0f647fdd9568c68a
Antoine Barthelemy <antoine@linux.com>
2022-01-14 17:40:30 +0100

    ex00

--- module_00/ex00/installer.sh
+++ module_00/ex00/installer.sh
@@ -6,7 +6,7 @@ else
 	export TARGET="Miniconda3-py37_4.10.3-Linux-x86_64.sh"
 fi
 curl -o /tmp/$TARGET https://repo.anaconda.com/miniconda/$TARGET
-bash /tmp/$TARGET -b -p $MINICONDA_PATH
+bash /tmp/$TARGET -u -b -p $MINICONDA_PATH
 grep -qxF 'export PATH=$MINICONDA_PATH:$PATH' ~/.${SHELL##*/}rc || echo 'export PATH=$MINICONDA_PATH:$PATH' >> ~/.${SHELL##*/}rc
 builtin type python
 python --version
