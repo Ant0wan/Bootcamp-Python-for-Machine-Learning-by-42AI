5efa61784660e8d864a51440506979a3370011dd
Antoine Barthelemy <antoine@linux.com>
2022-01-14 17:15:22 +0100

    ex00 module00 install file

--- module_00/ex00/installer.sh
+++ module_00/ex00/installer.sh
@@ -1,32 +1,11 @@
-#!/bin/bash
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    installer.sh                                       :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2019/11/04 09:09:25 by abarthel          #+#    #+#              #
-#    Updated: 2019/11/04 09:09:25 by abarthel         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-function install_python()
-{
-	curl -o /goinfre/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
-	if [ $? -ne 0 ]
-	then
-		printf "Failure: could not download miniconda.\n"
-		exit()
-	fi
-	bash /goinfre/miniconda.sh -b -p /goinfre/miniconda
-	if [ $? -eq 0 ]
-	then
-		printf "Python has been installed.\n"
-	else
-		printf "Failure: Python could not be installed.\n"
-		exit()
-	fi
-}
-
-install_python
+#!/usr/bin/env bash
+set -x
+export MINICONDA_PATH=/goinfre/miniconda3
+if [[ $OSTYPE == 'darwin'* ]]; then
+	export TARGET="Miniconda3-py37_4.10.3-MacOSX-x86_64.sh"
+else
+	export TARGET="Miniconda3-py37_4.10.3-Linux-x86_64.sh"
+fi
+curl -o /tmp/$TARGET https://repo.anaconda.com/miniconda/$TARGET
+bash /tmp/$TARGET -b -p $MINICONDA_PATH
+grep -qxF 'export PATH=\$MINICONDA_PATH:\$PATH' ~/.${SHELL##*/}rc || echo 'export PATH=\$MINICONDA_PATH:\$PATH' >> ~/.${SHELL##*/}rc
