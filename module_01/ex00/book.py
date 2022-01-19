0b4f5a865865243d1bdf1bc97ef88879099e96c4
Antoine Barthelemy <antoine@linux.com>
2022-01-19 23:27:36 +0100

    book class on the go

--- module_01/ex00/book.py
+++ module_01/ex00/book.py
@@ -26,14 +26,17 @@ class Book:
 
     @property
     def recipe_list(self):
+        """'Recipe_list' property"""
         return self.__recipe_list
 
     @property
     def last_update(self):
+        """'Last_update' property"""
         return self.__last_update
 
     @property
     def creation_date(self):
+        """'Creation_date' property"""
         return self.__creation_date
 
     @name.setter
@@ -55,14 +58,13 @@ class Book:
     @staticmethod
     def _only_dictoftype(lst):
         if isinstance(lst, dict):
-            for t in DISHTYPES:
-                if t not in lst.keys():
+            for dishtype in DISHTYPES:
+                if dishtype not in lst.keys():
                     raise ValueError('Can only have dishtypes keys')
-                if not isinstance(lst.get(t), list):
+                if not isinstance(lst.get(dishtype), list):
                     raise ValueError('Items must be of list type')
             return dict(lst)
-        else:
-            raise ValueError('Value must be dict')
+        raise ValueError('Value must be dict')
 
     @staticmethod
     def _isdate(val):
@@ -75,18 +77,18 @@ class Book:
     def get_recipe_by_name(self, name):
         """Print a recipe with the name `name` and return the instance"""
         only_str(name)
-        for t in DISHTYPES:
-            if self.__recipe_list.get(t):
-                if name is self.__recipe_list.get(t)[0].name:
-                    print(self.__recipe_list.get(t)[0])
-                    return self.__recipe_list.get(t)
+        for dishtype in DISHTYPES:
+            if self.__recipe_list.get(dishtype):
+                if name is self.__recipe_list.get(dishtype)[0].name:
+                    print(self.__recipe_list.get(dishtype)[0])
+                    return self.__recipe_list.get(dishtype)
         return None
 
     def get_recipes_by_types(self, recipe_type):
         """Get all recipe names for a given recipe_type """
         only_str(recipe_type)
         if recipe_type in DISHTYPES:
-            allnames = list()
+            allnames = []
             for i in self.__recipe_list.get(recipe_type):
                 allnames.append(i.name)
             if allnames:
