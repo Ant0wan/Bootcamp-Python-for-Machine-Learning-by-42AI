a2edae3f53c1f139d53e5677eb35fac903c2f845
Antoine Barthelemy <antoine@linux.com>
2022-01-21 00:41:55 +0100

    module 01 ex02 on the go for vectors

--- module_01/ex02/vector.py
+++ module_01/ex02/vector.py
@@ -9,5 +9,10 @@ class Vector:
     def __init__(self, vector=[]):
         self.__vector = list(vector)
 
+    @staticmethod
+    def _check_vector(vector):
+        if isinstance(vector, list) and (all(isinstance(value, float) for value in vector) or all(isinstance(value, list) for value in vector)):
+
+
 #    @property
 #    def get(self):
