763e60c8e53c0652d1e8b56dc490b1256c9c2131
Antoine Barthelemy <antoine@linux.com>
2022-01-21 00:30:46 +0100

    module 01 ex02 on the go for vectors

---
+++ module_01/ex02/test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Unit tests
+"""
+
+import unittest
+
+from vector import Vector
+
+class TestVectorClass(unittest.TestCase):
+    """Test Class for functions and constant from vector module"""
+
+    def test_init(self):
+        """Test vector constructor"""
+        print('hey')
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
---
+++ module_01/ex02/vector.py
@@ -0,0 +1,13 @@
+"""
+Vectors
+"""
+
+
+class Vector:
+    """Vector Class"""
+
+    def __init__(self, vector=[]):
+        self.__vector = list(vector)
+
+#    @property
+#    def get(self):
