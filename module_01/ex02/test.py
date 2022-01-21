6f4461b0123d435e131c4ba252adc90903943b78
Antoine Barthelemy <antoine@linux.com>
2022-01-21 11:47:35 +0100

    module01 ex02 vectors and test on the go

--- module_01/ex02/test.py
+++ module_01/ex02/test.py
@@ -10,9 +10,17 @@ from vector import Vector
 class TestVectorClass(unittest.TestCase):
     """Test Class for functions and constant from vector module"""
 
-    def test_init(self):
-        """Test vector constructor"""
-        print('hey')
+    def test_check_vector(self):
+        self.assertEqual(Vector._check_vector([1.2, 3.4]), [1.2, 3.4])
+        self.assertEqual(Vector._check_vector([0.0, 1.0, 2.0, 3.0]), [0.0, 1.0, 2.0, 3.0])
+        self.assertRaises(ValueError, Vector._check_vector, ['1.2', '3.4'])
+        self.assertRaises(ValueError, Vector._check_vector, ['', ''])
+        self.assertRaises(ValueError, Vector._check_vector, [])
+
+
+   # def test_init(self):
+   #     """Test vector constructor"""
+   #     self.
 
 
 if __name__ == '__main__':
--- module_01/ex02/vector.py
+++ module_01/ex02/vector.py
@@ -7,11 +7,24 @@ class Vector:
     """Vector Class"""
 
     def __init__(self, vector=[]):
-        self.__vector = list(vector)
+        self.__vector = _check_vector(vector)
 
     @staticmethod
-    def _check_vector(vector):
-        if isinstance(vector, list) and (all(isinstance(value, float) for value in vector) or all(isinstance(value, list) for value in vector)):
+    def _check_vector(vec):
+        """Check only 1 and 2 dimensions vectors"""
+        # Check if two
+            # check is list
+                # check is list of list
+                    # check each list of list is float
+
+        # Check if one
+        # check if list of float
+        if not vec:
+            raise ValueError("Vector cannot be empty")
+        if isinstance(vec, list) and all(isinstance(val, float) for val in vec):
+            return vec
+        raise ValueError("Vector must be list of floats or list of lists of floats")
+        # raise error
 
 
 #    @property
