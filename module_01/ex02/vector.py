13ff49866b0f9337cced3f6b65816b182031878c
Antoine Barthelemy <antoine@linux.com>
2022-01-23 19:09:54 +0100

    module 01 ex02 still mul tests .dot and .T methods

--- module_01/ex02/vector.py
+++ module_01/ex02/vector.py
@@ -185,10 +185,9 @@ class Vector:
         if not isinstance(other, Vector):
             raise TypeError('argurment must be of type Vector')
         if self.__shape[1] == other.shape[0]:
-            scalar_product = Vector(self.__shape[0] * other.shape[1])
-            #scalar_product = Vector((self.__shape[0], other.shape[1])) # use append
-            for column_index in range(self.__shape[0]):
-                for row_index in range(self.__shape[1]):
-                    scalar_product[column_index][] = self.values[row_index] * other.values[column_index]
+            scalar_product = self._reshape(self._arange(self.__shape[0] * other.shape[1]), self.__shape[0], other.shape[1])
+           # for column_index in range(self.__shape[0]):
+           #     for row_index in range(self.shape[1]):
+           #         scalar_product[column_index][] = self.values[row_index] * other.values[column_index]
+            return scalar_product
         raise TypeError(f"ValueError: shapes ({self.__shape[0]},{self.__shape[1]}) and ({other.shape[0]},{other.shape[1]}) not aligned: {self.__shape[1]} (dim 1) != {other.shape[0]} (dim 0)")
-        return scalar_product
