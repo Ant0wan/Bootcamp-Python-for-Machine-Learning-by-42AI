0d65ff48a1752b8f18616146f7a0327958699d3c
Antoine Barthelemy <antoine@linux.com>
2022-01-23 21:18:23 +0100

    module 01 ex02 still mul tests .dot and .T methods

--- module_01/ex02/vector.py
+++ module_01/ex02/vector.py
@@ -35,6 +35,11 @@ class Vector:
         return array
 
     def __repr__(self):
+        """Example:
+            array([[ 0  1  2  3  4]
+                   [ 5  6  7  8  9]
+                   [10 11 12 13 14]])
+        """
         return 'array(' + self.__str__('       ') + ')'
 
     def __add__(self, other):
@@ -191,6 +196,7 @@ class Vector:
          a.   0.0 3.0 5.0   b.  3.0 1.0     a[0][0]*b[0][0] + a[0][1]*b[1][0] + a[0][2]*b[2][0]   a[0][0]*b[0][1] + a[0][1]*b[1][1] + a[0][2]*b[2][1]
               5.0 5.0 2.0       4.0 2.0     a[1][0]*b[0][0] + a[1][1]*b[1][0] + a[1][2]*b[2][0]   a[1][0]*b[0][1] + a[1][1]*b[1][1] + a[1][2]*b[2][1]
                                 3.0 3.0
+
         """
         if not isinstance(other, Vector):
             raise TypeError('argurment must be of type Vector')
