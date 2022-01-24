839363b4d719b88ebcc1783c6f6dc532006b4444
Antoine Barthelemy <antoine@linux.com>
2022-01-24 21:20:04 +0100

    ex03 module 01 on the go

--- module_01/ex03/generator.py
+++ module_01/ex03/generator.py
@@ -1,3 +1,16 @@
 """
 Generator
 """
+
+def generator(text, sep=" ", option=None):
+    """Yields the resulting substrings split by sep
+
+    Keyword arguments:
+     text   -- text to be split
+     sep    -- string as a splitting parameter
+     option -- (optional) takes:
+         shuffle -- shuffles the list of words
+         unique  -- returns a list where each word appears only once
+         ordered -- alphabetically sorts the words
+    """
+    pass
