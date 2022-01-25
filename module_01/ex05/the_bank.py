df19709cdc9c10401cddecaa37212ce69d9f6b3b
Antoine Barthelemy <antoine@linux.com>
2022-01-25 23:24:39 +0100

    ex05 module 01 on the go

---
+++ module_01/ex05/the_bank.py
@@ -0,0 +1,18 @@
+"""
+Bank Account
+"""
+
+
+class Account:
+    """Account class"""
+
+    ID_COUNT = 1
+
+    def __init__(self, name, **kwargs):
+        self.id = self.ID_COUNT
+        self.name = name
+        self.__dict__.update(kwargs)
+        Account.ID_COUNT += 1
+
+    def transfer(self, amount):
+        self.value += amount
