9d395e53bf151705f0536f21e9c99bb94cd9c329
Antoine Barthelemy <antoine@linux.com>
2022-01-28 18:21:33 +0100

    module01 ex05 bank class on the go

--- module_01/ex05/the_bank.py
+++ module_01/ex05/the_bank.py
@@ -16,3 +16,27 @@ class Account:
 
     def transfer(self, amount):
         self.value += amount
+
+
+class Bank:
+        """The bank"""
+        def __init__(self):
+            self.account = []
+
+        def add(self, account):
+            self.account.append(account)
+
+        def transfer(self, origin, dest, amount):
+        """
+        @origin: int(id) or str(name) of the first account
+        @dest: int(id) or str(name) of the destination account
+        @amount: float(amount) amount to transfer
+        @return True if success, False if an error occured
+        """
+
+        def fix_account(self, account):
+        """
+        fix the corrupted account
+        @account: int(id) or str(name) of the account
+        @return True if success, False if an error occured
+        """
