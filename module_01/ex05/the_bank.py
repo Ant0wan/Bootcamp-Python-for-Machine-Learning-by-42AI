b3457f4484267d272486bcf64c0cc3c13f952341
Antoine Barthelemy <antoine@linux.com>
2022-01-29 22:18:47 +0100

    module01 ex05 bank class on the go

--- module_01/ex05/the_bank.py
+++ module_01/ex05/the_bank.py
@@ -19,24 +19,61 @@ class Account:
 
 
 class Bank:
-        """The bank"""
-        def __init__(self):
-            self.account = []
+    """The Bank Class
+        Checks Account:
+         - the right object
+         - not corrupted
+            define if a bank account is corrupted
+            . an even number of attributes
+            . an attribute starting with b
+            . no attribute starting with zip or addr
+            . no attribute name, id and value
+         - and stores enough money to complete the transfer
+            . invalid is amount < 0
 
-        def add(self, account):
-            self.account.append(account)
+        -> dir built-in function
+    """
 
-        def transfer(self, origin, dest, amount):
+    def __init__(self):
+        self.account = []
+
+    def add(self, account):
+        self.account.append(account)
+
+    @staticmethod
+    def _corrupted(account):
+        """Check whether an Account class object is corrupted
+          Corrupted if:
+            . an even number of attributes
+            . an attribute starting with b
+            . no attribute starting with zip or addr
+            . no attribute name, id and value
+        """
+        if (
+                not len(account.__dict__.keys()) % 2
+                or len(list(filter(lambda attr: attr.startswith('b'), account.__dict__.keys())))
+                or not len(list(filter(lambda attr: attr.startswith('zip'), account.__dict__.keys())))
+                or not len(list(filter(lambda attr: attr.startswith('addr'), account.__dict__.keys())))
+                or name in account.__dict__.keys()
+                or id in account.__dict__.keys()
+                or value in account.__dict__.keys()
+            ):
+            return True
+        return False
+
+    def transfer(self, origin, dest, amount):
         """
         @origin: int(id) or str(name) of the first account
         @dest: int(id) or str(name) of the destination account
         @amount: float(amount) amount to transfer
         @return True if success, False if an error occured
         """
+        pass
 
-        def fix_account(self, account):
+    def fix_account(self, account):
         """
         fix the corrupted account
         @account: int(id) or str(name) of the account
         @return True if success, False if an error occured
         """
+        pass
