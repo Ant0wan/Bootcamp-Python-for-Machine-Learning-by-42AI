2608388047678b24f3111c5f2f967318e0cab440
Antoine Barthelemy <antoine@linux.com>
2022-01-30 14:31:26 +0100

    module02 ex00 ft_filter

--- module_02/ex00/ft_filter.py
+++ module_02/ex00/ft_filter.py
@@ -1,3 +1,18 @@
 """
 filter function
 """
+
+
+def ft_filter(function_to_apply, iterable):
+    """Filter the result of function apply to all elements of the iterable.
+
+    Args:
+    function_to_apply: a function taking an iterable.
+    iterable: an iterable object (list, tuple, iterator).
+
+    Returns:
+    An iterable.
+    None if the iterable can not be used by the function.
+    """
+    for element in [x for x in iterable if function_to_apply(x)]:
+        yield element
