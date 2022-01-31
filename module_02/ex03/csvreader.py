c7e8fd6dc46b0065be096664d5bfc708357315ee
Antoine Barthelemy <antoine@linux.com>
2022-01-31 18:33:20 +0100

    module02 ex03 csvreader on the go

--- module_02/ex03/csvreader.py
+++ module_02/ex03/csvreader.py
@@ -4,30 +4,47 @@ ex03
 
 
 class CsvReader():
-    """CsvReader Class"""
+    """CsvReader Class context manager"""
 
 	def __init__(self, filename=None, sep=',', header=False, skip_top=0, skip_bottom=0):
-		... Your code ...
+        self.filename = filename
+        self.sep = sep
+        self.header = header
+        self.skip_top = skip_top
+        self.skip_bottom = skip_bottom
 
-	def __enter__(...):
-		... Your code ...
+	def __enter__(self):
+        self.file = open(self.filename, 'rt')
+        lines = self.file.read().split('\n')
+        if self.header:
+            self.header = lines.[0].split(self.sep)
+            self.data = [line.split(self.sep) for line in lines[1 + self.skip_top:self.skip_bottom]]
+            self.len = len(self.header)
+        else:
+            self.header = None
+            self.data = [line.split(self.sep) for line in lines[self.skip_top:self.skip_bottom]]
+            self.len = len(self.data[0])
+        if list(filter(lambda x: len(x) != self.len, self.data)):
+            raise AssertionError("Invalid csv file")
+        return self.file
 
-	def __exit__(...):
-		... Your code ...
+	def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.file
+            self.file.close()
 
 	def getdata(self):
-	""" Retrieves the data/records from skip_top to skip bottom.
-	Return:
-	-------
-		nested list (list(list, list, ...)) representing the data.
-	"""
-		... Your code ...
+	    """ Retrieves the data/records from skip_top to skip bottom.
+	    Return:
+	    -------
+	    	nested list (list(list, list, ...)) representing the data.
+	    """
+		return self.data
 
 	def getheader(self):
-	""" Retrieves the header from csv file.
-	Return:
-	-------
-		list: representing the data (when self.header is True).
-        None: (when self.header is False).
-	"""
-		... Your code ...
+	    """ Retrieves the header from csv file.
+	    Return:
+	    -------
+	    	list: representing the data (when self.header is True).
+            None: (when self.header is False).
+	    """
+        return self.header
