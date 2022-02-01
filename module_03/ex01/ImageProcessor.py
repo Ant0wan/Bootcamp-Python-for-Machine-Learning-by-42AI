48c63c4f30195799eaa3dea2438f85b0f00545b4
Antoine Barthelemy <antoine@linux.com>
2022-02-01 21:35:42 +0100

    module03 ex01 done linter

--- module_03/ex01/ImageProcessor.py
+++ module_03/ex01/ImageProcessor.py
@@ -4,5 +4,26 @@ ex01
 """
 
 import numpy
-import matplotlib.pyplot
-import matplotlib.image
+
+from matplotlib import image
+from matplotlib import pyplot
+
+
+class ImageProcessor:
+    """Load and display image as numpy array"""
+
+    @staticmethod
+    def load(path):
+        """Load image from path as numpy array"""
+        img = image.imread(path)
+        array = numpy.array(img)
+        shape = numpy.shape(array)
+        print(f"Image dimensions: {shape[0]} x {shape[1]}")
+        return array
+
+    @staticmethod
+    def display(array):
+        """Display image from array"""
+        pyplot.imshow(array)
+        pyplot.axis("off")
+        pyplot.show()
