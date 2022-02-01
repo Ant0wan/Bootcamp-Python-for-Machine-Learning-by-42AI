d391284591df40c4a9ba8ef0fef555e8360e189a
Antoine Barthelemy <antoine@linux.com>
2022-02-01 17:03:07 +0100

    module 02 ex04 on the go

--- module_02/ex04/build.sh
+++ module_02/ex04/build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 python3 -m pip install --upgrade pip wheel setuptools
 python setup.py bdist_wheel
+VERSION="$(grep version setup.py | cut -d "'" -f2)"
+PACKAGE_NAME="$(grep name setup.py | cut -d "'" -f2)"
+PACKAGE_NAME=${PACKAGE_NAME//[-]/_}
+tar -czvf dist/${PACKAGE_NAME}-${VERSION}.tar.gz dist/${PACKAGE_NAME}-${VERSION}-py3-none-any.whl
