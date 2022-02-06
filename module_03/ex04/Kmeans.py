71e551ed05f904aaa812680e6c1caaeb1738af3b
Antoine Barthelemy <antoine@linux.com>
2022-02-06 18:20:35 +0100

    module 03 ex04 parsing with argparse

--- module_03/ex04/Kmeans.py
+++ module_03/ex04/Kmeans.py
@@ -2,9 +2,11 @@
 """
 ex04
 input: python Kmeans.py filepath='../ressources/solar_system_census.csv' ncentroid=4 max_iter=30
+tuto: https://www.youtube.com/watch?v=EItlUEPCIzM
 """
 
 import argparse
+import numpy
 import sys
 
 
@@ -18,44 +20,28 @@ class KmeansClustering:
         """
         Run the K-means clustering algorithm.
         For the location of the initial centroids, random pick ncentroids from the dataset.
-        Args:
-        -----
-          X: has to be an numpy.ndarray, a matrice of dimension m * n.
-        Return:
-        -------
-          None.
-        Raises:
-        -------
-          This function should not raise any Exception.
         """
-        #... your code ...
         pass
 
     def predict(self, X):
         """
         Predict from wich cluster each datapoint belongs to.
-        Args:
-        -----
-          X: has to be an numpy.ndarray, a matrice of dimension m * n.
-        Return:
-        -------
-          the prediction has a numpy.ndarray, a vector of dimension m * 1.
-        Raises:
-        -------
-          This function should not raise any Exception.
         """
-        #... your code ...
         pass
 
 
 def main(*args):
     parser = argparse.ArgumentParser(prog='kmeans', description='Implementation of a basic Kmeans algorithm.')
+
     parser.add_argument('--version', action='version', version='%(prog)s 0.1')
     parser.add_argument('-f', '-filepath', '--filepath', nargs=1, type=argparse.FileType('r'), required=True)
-    #parser.add_argument('-f', '-filepath', '--filepath', nargs=1, type=argparse.FileType('r'), default=sys.stdin)
-    parser.add_argument('-n', '-ncentroid', '--ncentroid', nargs=1, type=int, required=True)
-    parser.add_argument('-m', '-max_iter', '--max_iter', nargs=1, type=int, required=True)
+    parser.add_argument('-n', '-ncentroid', '--ncentroid', nargs=1, type=int)
+    parser.add_argument('-m', '-max_iter', '--max_iter', nargs=1, type=int)
+
     args = parser.parse_args()
+
+    kmean = KmeansClustering(args.max_iter, args.ncentroid)
+    kmean.fit()
 
 
 if __name__ == '__main__':
