8f6eaea030cc66ca009d0da1c8a3ad55a959919e
Antoine Barthelemy <antoine@linux.com>
2022-02-07 17:59:36 +0100

    module03 ex04 linter on the go

--- module_03/ex04/Kmeans.py
+++ module_03/ex04/Kmeans.py
@@ -20,27 +20,54 @@ class KmeansClustering:
         self.ncentroid = ncentroid # number of centroids
         self.max_iter = max_iter # number of max iterations to update the centroids
         self.centroids = [] # values of the centroids
+        #self.headers = []
+        #self.kmeans = None
 
-    def fit(self, X):
+    def fit(self, X: pandas.core.frame.DataFrame):
         """
         Run the K-means clustering algorithm.
         For the location of the initial centroids, random pick ncentroids from the dataset.
         """
-        #km = KMeans(init='random', n_clusters=self.ncentroid, n_init=self.max_iter)
-        pass
+        self.kmeans = KMeans(n_clusters=self.ncentroid, n_init=self.max_iter)
+        self.headers = list(X.columns)
+        self.kmeans.fit(X[self.headers])
 
-    def predict(self, X):
+    def predict(self, X: pandas.core.frame.DataFrame):
         """
         Predict from wich cluster each datapoint belongs to.
         """
-        pass
+        self.kmeans.predict(X)
+        self.centroids = self.kmeans.cluster_centers_
+        return self.centroids
 
 
-def main(*args):
+def plot(points, km, ncentroid):
+    """
+    Plot clusters on 3D graph and display centroid info
+    """
+    fig = pyplot.figure()
+    ax = fig.add_subplot(projection='3d')
+
+    markers = iter(('o', 'v', 's', 'D', '+'))
+
+    for centroid in range(ncentroid):
+        mask = km.kmeans.labels_ == centroid
+        print(sum(mask), 'elements in centroid', centroid, 'with coordinates', km.kmeans.cluster_centers_[centroid])
+        ax.scatter(points[km.headers[0]][mask], points[km.headers[1]][mask], points[km.headers[2]][mask], marker=next(markers))
+
+    ax.scatter(km.kmeans.cluster_centers_[:,0], km.kmeans.cluster_centers_[:,1], km.kmeans.cluster_centers_[:,2], c='r', marker='X')
+
+    ax.set_xlabel(km.headers[0])
+    ax.set_ylabel(km.headers[1])
+    ax.set_zlabel(km.headers[2])
+
+    pyplot.show()
+
+
+def main():
     parser = argparse.ArgumentParser(prog='kmeans', description='Implementation of a basic Kmeans algorithm.')
 
     parser.add_argument('--version', action='version', version='%(prog)s 0.1')
-    #parser.add_argument('-f', '-filepath', '--filepath', nargs=1, type=argparse.FileType('r'), required=True)
     parser.add_argument('-f', '-filepath', '--filepath', type=str, required=True)
     parser.add_argument('-n', '-ncentroid', '--ncentroid', type=int)
     parser.add_argument('-m', '-max_iter', '--max_iter', type=int)
@@ -52,38 +79,12 @@ def main(*args):
     mask = raw.columns.str.match("Unnamed")
     points = raw.loc[:,~mask]
 
-    # Get header list
-    headers = list(points.columns)
+    # Kmeans clustering
+    km = KmeansClustering(args.max_iter, args.ncentroid)
+    km.fit(points)
 
-    # kmeans fit
-    km = KMeans(n_clusters=args.ncentroid, n_init=args.max_iter)
-    km.fit(points[headers])
-    # Predict
-#    predicted = km.predict(points)
-#    # Update data with cluster number
-#    points['cluster'] = predicted
-    #print(km.labels_)
-
-
-    # Plot list
-    fig = pyplot.figure()
-    ax = fig.add_subplot(projection='3d')
-
-    markers = iter(('o', 'v', 's', 'D', '+'))
-
-    for centroid in range(args.ncentroid):
-        mask = km.labels_ == centroid
-        print(sum(mask), 'elements in centroid', centroid, 'with coordinates', km.cluster_centers_[centroid])
-        ax.scatter(points[headers[0]][mask], points[headers[1]][mask], points[headers[2]][mask], marker=next(markers))
-
-    ax.scatter(km.cluster_centers_[:,0], km.cluster_centers_[:,1], km.cluster_centers_[:,2], c='r', marker='X')
-
-    ax.set_xlabel(headers[0])
-    ax.set_ylabel(headers[1])
-    ax.set_zlabel(headers[2])
-
-    pyplot.show()
-
+    # Plot clusters
+    plot(points, km, args.ncentroid)
 
 
 if __name__ == '__main__':
