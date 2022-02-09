f4c080df5f75312559e69673565f61bb417116a2
Antoine Barthelemy <antoine@linux.com>
2022-02-10 00:03:04 +0100

    module 04 ex01

---
+++ module_04/ex01/FileLoader.py
@@ -0,0 +1,25 @@
+# pylint: disable=invalid-name
+"""
+ex00
+"""
+
+import os
+import pandas
+
+
+class FileLoader:
+    """FileLoader class"""
+
+    @staticmethod
+    def load(path: os.path) -> pandas.DataFrame:
+        """Take filepath and return panda data frame"""
+        data = pandas.read_csv(path)
+        # pylint: disable=no-member
+        print(f"Loading dataset of dimensions \
+{data.shape[0]} x {data.shape[1]}")
+        return data
+
+    @staticmethod
+    def display(df: pandas.DataFrame, n):
+        """Display data frame n first lines"""
+        print(df.head(n))
---
+++ module_04/ex01/YoungestFellah.py
@@ -0,0 +1,11 @@
+# pylint: disable=invalid-name
+"""
+ex01
+"""
+
+
+def youngfellah(df, year) -> dict:
+    """Get the name of the youngest
+    woman and man for the given year.
+    """
+    pass
