073b431fb7b71bbdba6b4b50893be088ce2b4e01
Antoine Barthelemy <antoine@linux.com>
2022-02-11 18:51:01 +0100

    module 04 ex 06

---
+++ module_04/ex06/MyPlotLib.py
@@ -0,0 +1,35 @@
+# pylint: disable=invalid-name
+"""
+ex06
+"""
+
+import pandas
+
+
+class MyPlotLib:
+    """This class implements different plotting methods"""
+
+    def __init__(self, data: pandas.DataFrame, names: list):
+        self.data = data
+        self.names = names
+
+    @staticmethod
+    def histogram(data: pandas.DataFrame, features: list):
+        """lots one histogram for each numerical feature in
+        the list"""
+
+    @staticmethod
+    def density(data: pandas.DataFrame, features: list):
+        """plots the density curve of each numerical feature in
+        the list"""
+
+    @staticmethod
+    def pair_plot(data: pandas.DataFrame, features: list):
+        """lots a matrix of subplots
+        """
+
+    @staticmethod
+    def box_plot(data: pandas.DataFrame, features: list):
+        """displays a box plot for each numerical
+        variable in the dataset
+        """
