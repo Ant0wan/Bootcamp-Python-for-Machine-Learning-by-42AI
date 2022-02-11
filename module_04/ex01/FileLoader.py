7c8bfbdb2a6ad6f5d43528761d9bf755b36e0bcf
Antoine Barthelemy <antoine@linux.com>
2022-02-11 09:14:28 +0100

    module04 ex02 done

--- module_04/ex01/FileLoader.py
+++ module_04/ex01/FileLoader.py
@@ -1,6 +1,6 @@
 # pylint: disable=invalid-name
 """
-ex00
+ex01
 """
 
 import os
@@ -13,13 +13,13 @@ class FileLoader:
     @staticmethod
     def load(path: os.path) -> pandas.DataFrame:
         """Take filepath and return panda data frame"""
-        data = pandas.read_csv(path)
+        data = pandas.read_csv(path, usecols=[2, 3, 9])
         # pylint: disable=no-member
         print(f"Loading dataset of dimensions \
 {data.shape[0]} x {data.shape[1]}")
         return data
 
     @staticmethod
-    def display(df: pandas.DataFrame, n):
+    def display(df, n):
         """Display data frame n first lines"""
         print(df.head(n))
--- module_04/ex01/YoungestFellah.py
+++ module_04/ex01/YoungestFellah.py
@@ -8,4 +8,8 @@ def youngfellah(df, year) -> dict:
     """Get the name of the youngest
     woman and man for the given year.
     """
-    pass
+    ymask = df['Year'] == year
+    df = df[ymask]
+    fmask = df['Sex'] == 'F'
+    mmask = df['Sex'] == 'M'
+    return {'f': df[fmask].min().get('Age'), 'm': df[mmask].min().get('Age')}
