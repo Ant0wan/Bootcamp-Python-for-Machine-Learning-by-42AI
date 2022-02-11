ddd60adda32adc190f4280dece275e801d2735ce
Antoine Barthelemy <antoine@linux.com>
2022-02-11 11:42:15 +0100

    module04 ex02

---
+++ module_04/ex02/ProportionBySport.py
@@ -0,0 +1,14 @@
+# pylint: disable=invalid-name
+"""
+ex02
+"""
+
+import pandas
+
+
+def proportionBySport(df: pandas.DataFrame, year: int, sport: str, gender: str) -> float:
+    """Displaying the proportion of participants who played
+    a given sport, among the participants of a given genders.
+    """
+    #Do stuff here
+    return percentage
