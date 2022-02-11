a94c4a86972926518586d3d3e7fdd3aa9a2b68c1
Antoine Barthelemy <antoine@linux.com>
2022-02-11 17:40:48 +0100

    module04 ex03 pylint on the go

---
+++ module_04/ex03/HowManyMedals.py
@@ -0,0 +1,37 @@
+# pylint: disable=invalid-name
+"""
+ex02
+"""
+
+import numpy
+import pandas
+
+
+def howManyMedals(df: pandas.DataFrame, name: str) -> dict:
+    """Returns a dictionary of dictionaries giving
+    the number and type of medals for each year
+    during which the participant won medals.
+    """
+    # Filter df with only participant's
+    mask = df['Name'] == name
+    participant_df = df[mask]
+
+    # Medal masks
+    gold = participant_df['Medal'] == 'Gold'
+    silver = participant_df['Medal'] == 'Silver'
+    bronze = participant_df['Medal'] == 'Bronze'
+
+    # Participant df only medal winning years
+    medals_df = participant_df[gold | silver | bronze]
+    years = medals_df['Year'].unique()
+
+    # Sum medals for each year
+    medals = dict()
+    for year in years:
+        mask = participant_df['Year'] == year
+        year_df = participant_df[mask]
+        medals[year] = {
+                        'G': len(year_df[year_df['Medal'] == 'Gold']),
+                        'S': len(year_df[year_df['Medal'] == 'Silver']),
+                        'B': len(year_df[year_df['Medal'] == 'Bronze']) }
+    return medals
