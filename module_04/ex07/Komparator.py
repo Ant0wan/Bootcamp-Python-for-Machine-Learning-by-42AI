00606c3b6fe06348e670416f1851ffe32f1035dd
Antoine Barthelemy <antoine@linux.com>
2022-02-13 15:01:11 +0100

    module 04 ex07

--- module_04/ex07/Komparator.py
+++ module_04/ex07/Komparator.py
@@ -6,11 +6,15 @@ ex07
 import pandas
 import matplotlib.pyplot
 
+from MyPlotLib import MyPlotLib
+
+#"ID","Name","Sex","Age","Height","Weight","Team","NOC","Games","Year","Season","City","Sport","Event","Medal"
+
 
 class Komparator:
     """Komparator class"""
-    def __init__(self, data):
-        self.data = pandas.DataFrame
+    def __init__(self, data: pandas.DataFrame):
+        self.data = data
 
     def compare_box_plots(self, categorical_var, numerical_var):
         """displays a box plot with several boxes to
@@ -18,13 +22,31 @@ class Komparator:
         variable changes if we only consider the subpopulation
         which belongs to each category
         """
+   #     plt = MyPlotLib()
+   #     plt.box_plot(self.data, [categorical_var, numerical_var])
+
 
     def density(self, categorical_var, numerical_var):
         """displays the density of the numerical variable"""
-        pandas.DataFrame(data[features]).plot(kind='density')
-        matplotlib.pyplot.show()
+        #pandas.DataFrame(data[features]).plot(kind='density')
+        #matplotlib.pyplot.show()
+#        mpl = MyPlotLib()
+#        df = self.data
+#        mpl.density(df, ["Sex", "Height"])
 
     def compare_histograms(self, categorical_var, numerical_var):
         """plots the numerical variable in a separate
         histogram for each category
         """
+        categories = list(self.data[categorical_var].unique())
+        ncat = len(categories)
+
+        fig, axs = matplotlib.pyplot.subplots(ncols=ncat)
+
+        for index, cat in enumerate(categories):
+            mask = self.data[categorical_var] == cat
+            axs[index].set_title(cat)
+            axs[index].hist(self.data[mask][numerical_var].dropna())
+            axs[index].grid()
+
+        matplotlib.pyplot.show()
