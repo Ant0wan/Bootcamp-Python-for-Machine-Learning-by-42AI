43d7045b2e1f6ce9635cd3bc494796d758258632
Antoine Barthelemy <antoine@linux.com>
2022-02-13 13:09:50 +0100

    module 04 ex06 done

--- module_04/ex06/MyPlotLib.py
+++ module_04/ex06/MyPlotLib.py
@@ -14,8 +14,8 @@ class MyPlotLib:
         """lots one histogram for each numerical feature in
         the list"""
         nfeatures = len(features)
+        # pylint: disable=unused-variable
         fig, axs = matplotlib.pyplot.subplots(ncols=nfeatures)
-        fig.suptitle('Histograms')
         for hist_index in range(nfeatures):
             axs[hist_index].set_title(features[hist_index])
             axs[hist_index].hist(data[features[hist_index]].dropna())
@@ -26,14 +26,21 @@ class MyPlotLib:
     def density(data: pandas.DataFrame, features: list):
         """plots the density curve of each numerical feature in
         the list"""
+        pandas.DataFrame(data[features]).plot(kind='density')
+        matplotlib.pyplot.show()
 
     @staticmethod
     def pair_plot(data: pandas.DataFrame, features: list):
-        """lots a matrix of subplots
-        """
+        """lots a matrix of subplots"""
+        pandas.plotting.scatter_matrix(data[features])
+        matplotlib.pyplot.show()
 
     @staticmethod
     def box_plot(data: pandas.DataFrame, features: list):
         """displays a box plot for each numerical
         variable in the dataset
         """
+        # pylint: disable=unused-variable
+        fig, axs = matplotlib.pyplot.subplots()
+        axs.boxplot(data[features].dropna(), labels=features)
+        matplotlib.pyplot.show()
